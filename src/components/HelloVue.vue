218c6d84065662007dc638ee5597da3bc7b31dca
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-13 12:32:13 +0700

    learn print hello world

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<script>
+    console.info("Helo vue")
+</script>
+
+<template>
+    <h1 class="hello">Hello Vue</h1>
+</template>
+
+<style scoped>
+    .hello {
+        color: green;
+    }
+</style>
