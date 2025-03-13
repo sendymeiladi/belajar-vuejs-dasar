3660fa82763ed005659a5593ec11b5695ffdb6ed
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-13 14:20:19 +0700

    learn JS Expression Template

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -6,10 +6,16 @@
         id: "hello"
     };
     const buttonDisable = true;
+
+    function helloFunction() {
+        return "Hello World";
+    }
 </script>
 
 <template>
-    <h1 v-bind="data">{{ helu}}</h1>
+    <h1 v-bind="data">{{helu.toUpperCase()}}</h1>
+    <h2>{{ 100 * 200 }}</h2>
+    <h2>{{ helloFunction() }}</h2>
     <div :class="data.class" v-html="helu"></div>
     <button :disabled="buttonDisable">This is Button</button>
 </template>
