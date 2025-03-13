6109ddca36bfe1c4ddcc6df783e4cc98f1e09ed7
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-13 14:13:06 +0700

    learn Template Atributes

--- src/components/HelloVue.vue
+++ src/components/HelloVue.vue
@@ -1,11 +1,17 @@
 <script setup>
     console.info("Helo vue");
     const helu = "<h1>Hello Vue</h1>";
+    const data = {
+        class: "hello",
+        id: "hello"
+    };
+    const buttonDisable = true;
 </script>
 
 <template>
-    <h1 class="hello">{{ helu}}</h1>
-    <div v-html="helu"></div>
+    <h1 v-bind="data">{{ helu}}</h1>
+    <div :class="data.class" v-html="helu"></div>
+    <button :disabled="buttonDisable">This is Button</button>
 </template>
 
 <style scoped>
