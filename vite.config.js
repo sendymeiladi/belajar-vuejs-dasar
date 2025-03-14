988115292d67ebfb10292aeb106f3be9a1371d30
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-14 11:19:03 +0700

    learn Reactive

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         index : "index.html",
         hello : "hello.html",
         counter : "counter.html",
+        say_hello : "say-hello.html",
       }
     }
   }
