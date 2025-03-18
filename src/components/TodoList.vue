737c106d8ba6dc90b894d32823c6f48bf6df5600
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-18 11:58:03 +0700

    learn List Rendering

---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<script setup>
+const items = [
+    {
+        id: 1,
+        text: "Learn Vue 3",
+        done: true
+    },
+    {
+        id:2,
+        text: "Learn Vue Router",
+        done: false
+    },
+    {
+        id:3,
+        text: "Learn Vue Pinia",
+        done:false
+    },
+    {
+        id:4,
+        text: "Learn Vue Pinia 2",
+        done:false
+    }
+];
+</script>
+
+<template>
+    <h1>Todo List</h1>
+    <template v-for="item in items" >
+        <ul :key="item.id" v-if="!item.done">
+            <li v-for="(value, key) in item">
+                {{ key }} : {{ value }}
+            </li>
+        </ul>
+    </template>
+
+
+    <h1 v-for="i in 10">Hello {{ i }}</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/todolist.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import Todolist from "./components/TodoList.vue";
+
+createApp(Todolist).mount("#app");
