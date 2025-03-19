aae4728d1ba61b646e8ad56671e6991eab230ffd
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-19 10:43:09 +0700

    Learn Template Refs

---
+++ src/components/Note.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { reactive, ref, useTemplateRef } from 'vue';
+
+
+const notes = reactive([]);
+const note = ref("");
+const noteInput = useTemplateRef("noteInput");
+const noteList = useTemplateRef("noteList");
+
+function addNote(){
+    notes.push(note.value);
+    note.value = "";
+    noteInput.value.focus();
+    if(noteList.value){
+        noteList.value.forEach((li) => {
+            console.info(li.textContent);
+        })
+    }
+}
+</script>
+
+<template>
+    <h1>Buat Note</h1>
+    <div>
+        <input ref="noteInput" v-model="note" type="text" placeholder="Tulis note di sini">
+        <button @click="addNote">Tambah Note</button>
+    </div>
+
+    <h1>Daftar Note</h1>
+    <ul>
+        <li v-for="note in notes" ref="noteList">{{ note }}</li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/note.js
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import note from "./components/Note.vue";
+
+createApp(note).mount("#app");
