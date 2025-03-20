abec6f72e6e9a572b98cffef2cddbef87b4d7361
Sendy Meiladi <sendymeiladi.28@gmail.com>
2025-03-20 12:07:14 +0700

    Learn Component Model

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { reactive } from 'vue';
+import ContactForm from './ContactForm.vue';
+import ContactList from './ContactList.vue';
 
 
 const contact = reactive({
@@ -13,52 +15,13 @@ const contact = reactive({
 </script>
 
 <template>
-    <h1>Contact Us</h1>
-    <form>
-        <div>
-            <label for="name">Name
-                <input type="text" id="name" v-model="contact.name">
-            </label>
-        </div>
-        <div>
-            <label for="email">Email
-                <input type="text" id="email" v-model="contact.email">
-            </label>
-        </div>
-        <div>
-            <label for="age">Age
-                <input type="number" id="age" v-model.number="contact.age">
-            </label>
-        </div>
-        <div>
-            <label for="type">Type
-                <select id="type" name="type" v-model="contact.type">
-                    <option value="Regular">Regular</option>
-                    <option value="VIP">VIP</option>
-                </select>
-            </label>
-        </div>
-        <div>
-            <label for="complain">Complain
-                <input type="checkbox" id="complain" v-model="contact.complain">
-            </label>
-        </div>
-        <div>
-            <label for="message">Message
-                <textarea name="message" id="message" v-model="contact.message"></textarea>
-            </label>
-        </div>
-    </form>
-
-    <h1>Preview</h1>
-    <div>
-        <p>Name : {{ contact.name }}</p>
-        <p>Email : {{ contact.email }}</p>
-        <p>Age : {{ contact.age }}</p>
-        <p>Type : {{ contact.type }}</p>
-        <p>Complain : {{ contact.complain }}</p>
-        <p>Message : {{ contact.message }}</p>
-    </div>
+    <ContactForm v-model="contact" />
+    <ContactList v-model:name.uppercase="contact.name" 
+                v-model:email="contact.email"
+                v-model:age="contact.age"
+                v-model:type="contact.type"
+                v-model:complain="contact.complain"
+                v-model:message="contact.message"/>
 </template>
 
 <style scoped>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,53 @@
+<script setup>
+const model = defineModel({
+    type: Object,
+    required: true,
+    get: (value) => {
+        value.email = value.email.toUpperCase();
+        return value;
+    }
+});
+</script>
+
+<template>
+    <h1>Contact Us</h1>
+    <form>
+        <div>
+            <label for="name">Name
+                <input type="text" id="name" v-model="model.name">
+            </label>
+        </div>
+        <div>
+            <label for="email">Email
+                <input type="text" id="email" v-model="model.email">
+            </label>
+        </div>
+        <div>
+            <label for="age">Age
+                <input type="number" id="age" v-model.number="model.age">
+            </label>
+        </div>
+        <div>
+            <label for="type">Type
+                <select id="type" name="type" v-model="model.type">
+                    <option value="Regular">Regular</option>
+                    <option value="VIP">VIP</option>
+                </select>
+            </label>
+        </div>
+        <div>
+            <label for="complain">Complain
+                <input type="checkbox" id="complain" v-model="model.complain">
+            </label>
+        </div>
+        <div>
+            <label for="message">Message
+                <textarea name="message" id="message" v-model="model.message"></textarea>
+            </label>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
